4bc977938cafff1bc409adc5b6de8c44178b6eff
martin <martin.enkelvegan@gmail.com>
2021-03-22 20:06:56 +0100

    improved rfid with display code

--- sketch_rfid_with_display/sketch_rfid_with_display.ino
+++ sketch_rfid_with_display/sketch_rfid_with_display.ino
@@ -1,70 +1,25 @@
 #include <SPI.h>
 #include <MFRC522.h>
 #include <LedControl.h>
-
-// Include the required Arduino libraries:
 #include <MD_Parola.h>
 #include <MD_MAX72xx.h>
-#include <SPI.h>
 
-#define SS_PIN 10
-#define RST_PIN 9
+#define RFID_SS_PIN 10
+#define RFID_RST_PIN 9
 
-// Define hardware type, size, and output pins:
+#define DISPLAY_CS_PIN 3
+#define DISPLAY_DATA_PIN 4
+#define DISPLAY_CLK_PIN 5
+
 #define HARDWARE_TYPE MD_MAX72XX::FC16_HW
-#define MAX_DEVICES 1
-#define CS_PIN 3
-#define DATA_PIN 4
-#define CLK_PIN 5
+#define NUMBER_OF_DISPLAYS 1
 
-MFRC522 mfrc522(SS_PIN, RST_PIN);
-
-boolean authenticated = false;
-
-MD_Parola myDisplay = MD_Parola(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
+MFRC522 mfrc522(RFID_SS_PIN, RFID_RST_PIN);
+MD_Parola myDisplay = MD_Parola(HARDWARE_TYPE, DISPLAY_DATA_PIN, DISPLAY_CLK_PIN, DISPLAY_CS_PIN, NUMBER_OF_DISPLAYS);
 
 void setup() {
   setUpRfidScanner();
   setUpDisplay();
-}
-
-void loop() {
-  if (authenticated) {
-    if (myDisplay.displayAnimate()) {
-      myDisplay.displayReset();
-      authenticated = false;
-    }
-  }
-
-  if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) {
-    return;
-  }
-
-  String cardId = getCardId();
-  if (isAuthorized(cardId)) {
-    myDisplay.displayText("Access Granted", PA_CENTER, 30, 0, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
-    authenticated = true;
-  } else {
-    myDisplay.setTextAlignment(PA_CENTER);
-    myDisplay.print("X");
-    delay(1000);
-    myDisplay.displayClear();
-  }
-}
-
-boolean isAuthorized(String cardId) {
-  return cardId == "67 FA 80 C9";
-}
-
-String getCardId() {
-  String content = "";
-  for (byte i = 0; i < mfrc522.uid.size; i++) {
-    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-    content.concat(String(mfrc522.uid.uidByte[i], HEX));
-  }
-
-  content.toUpperCase();
-  return content.substring(1);
 }
 
 void setUpRfidScanner() {
@@ -77,4 +32,42 @@ void setUpDisplay() {
   myDisplay.begin();
   myDisplay.setIntensity(0);
   myDisplay.displayClear();
+}
+
+void loop() {
+  animateText();
+
+  if (noCardIsNearBy()) {
+    return;
+  }
+
+  if (cardIdIsValid(getCardId())) {
+    myDisplay.displayText("Access Granted", PA_CENTER, 30, 0, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
+  } else {
+    myDisplay.displayText("Access Denied", PA_CENTER, 30, 0, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
+  }
+}
+
+void animateText() {
+  if (myDisplay.displayAnimate()) {
+    myDisplay.displayClear();
+  }
+}
+
+boolean noCardIsNearBy() {
+  return !mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial();
+}
+
+boolean cardIdIsValid(String cardId) {
+  return cardId == "67 FA 80 C9";
+}
+
+String getCardId() {
+  String content = "";
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  content.toUpperCase();
+  return content.substring(1);
 }
